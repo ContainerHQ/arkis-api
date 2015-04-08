076fea515c9d044ef0d4a003335ddfd3c73fae18
Adrien Folie <folie.adrien@gmail.com>
2015-04-08 11:19:26 +0200

    add todo

--- routes/upgrade/index.js
+++ routes/upgrade/index.js
@@ -3,5 +3,6 @@ var DockerProxy = require('../../lib/docker');
 var docker = new DockerProxy();
 
 module.exports = function(req, socket, head) {
+    // TODO: check route, write 404 if invalid. Close socket
     docker.hijack(req, socket);
 }
