1133a451642c43508b99de7b639ecbbe4adf3354
Adrien Folie <folie.adrien@gmail.com>
2015-04-09 04:50:14 +0200

    cleanup certs parsing

--- routes/upgrade/index.js
+++ routes/upgrade/index.js
@@ -5,7 +5,6 @@ var dockerHost = new docker.Host();
 module.exports = function(req, socket, head) {
     // TODO: check route, write 404 if invalid. Close socket
 
-    console.log(req.headers);
     var proxy = new docker.Proxy(req, dockerHost);
 
     proxy.hijack(socket);
