0ad7862668a0fd7833590ec14440c71a34c645e3
Adrien Folie <folie.adrien@gmail.com>
2015-04-09 13:28:45 +0000

    init proxy in a middleware

--- routes/docker/index.js
+++ routes/docker/index.js
@@ -4,15 +4,17 @@ var _ = require('underscore'),
     api = require('./api'),
     docker = require('../../lib/docker');
 
-var dockerHost = new docker.Host();
+var dockerHost = new docker.Host(),
+    router = express.Router();
+
+router.use(function(req, res, next) {
+    req.proxy = new docker.Proxy(req, dockerHost);
+    next();
+});
 
 function proxyRequest(req, res) {
-    var proxy = new docker.Proxy(req, dockerHost);
-
-    proxy.redirect().pipe(res);
+    req.proxy.redirect().pipe(res);
 }
-
-var router = express.Router();
 
 _.each(api, function(routes, method) {
     _.each(routes, function(route) {
@@ -21,15 +23,13 @@ _.each(api, function(routes, method) {
 });
 
 router.get('/version', function(req, res) {
-    var proxy = new docker.Proxy(req, dockerHost);
-
-    proxy.redirect()
-          .pipe(es.split())
-          .pipe(es.parse())
-          .pipe(es.map(function(data, cb) {
-              data['ApiVersion'] += ' (Docker Proxy)';
-              res.send(data);
-          }));
+    req.proxy.redirect()
+        .pipe(es.split())
+        .pipe(es.parse())
+        .pipe(es.map(function(data, cb) {
+          data['ApiVersion'] += ' (Docker Proxy)';
+          res.send(data);
+        }));
 });
 
 module.exports = router;
