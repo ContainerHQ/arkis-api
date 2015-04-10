0581bd60d75dc8d15ba9636ae19152ea5f6aca3a
Adrien Folie <folie.adrien@gmail.com>
2015-04-10 13:49:22 +0000

    fix typo mistake

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ let port = process.env.PORT || 4000;
 
 app
   .use(bodyParser.json())
- .use(bodyParser.urlencoded({ extended: true }))
- .use(multer())
- .use(morgan('combined'))
- .use('/v1.14', routes.docker)
- .use('/v1.15', routes.docker)
- .use('/v1.16', routes.docker)
- .use('/v1.17', routes.docker)
- .listen(port, () => {
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(multer())
+  .use(morgan('combined'))
+  .use('/v1.14', routes.docker)
+  .use('/v1.15', routes.docker)
+  .use('/v1.16', routes.docker)
+  .use('/v1.17', routes.docker)
+  .listen(port, () => {
      console.log('Listenning on port: %s', port);
- })
- .on('upgrade', routes.upgrade);
+  })
+  .on('upgrade', routes.upgrade);
