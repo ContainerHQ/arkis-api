913db107379c990b55f21c2c7ada9bbe66f7f4e3
Adrien Folie <folie.adrien@gmail.com>
2015-04-10 05:13:30 +0200

    add info to test proxy

--- test/lib/docker/proxy.js
+++ test/lib/docker/proxy.js
@@ -4,4 +4,8 @@ describe('Docker Proxy', () => {
   //
   // TEST WITH A FAKE SERVER, WITH FAKE REQ
   // TEST STREAM, HIJACK AND CO
+  //
+  // N.B. To create a fake req:
+  // Req should implemente methods like
+  // a duplex stream. Create a support/req.js file.
 });
