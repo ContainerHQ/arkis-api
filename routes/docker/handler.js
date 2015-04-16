c11b52f0f97ae8b7d42145741211c2225f1dc610
Adrien Folie <folie.adrien@gmail.com>
2015-04-16 05:11:41 +0200

    add no content handler

--- routes/docker/handler.js
+++ routes/docker/handler.js
@@ -2,11 +2,6 @@ module.exports.notImplemented = function(req, res) {
   res.status(404).json('Not yet implemented.');
 };
 
-// TODO:
-// Use middleware to merge req.opts = query+body;
-
-// Merge sendTo and streamTo : sendTo(res, ...args);
-
 module.exports.sendTo = function(res, callback) {
   return function (err, data) {
     if (err) {
@@ -22,10 +17,18 @@ module.exports.sendTo = function(res, callback) {
 module.exports.streamTo = function(res, type='application/json') {
   return function (err, data) {
     if (err) {
-      console.log(err);
       return res.status(err.statusCode).send(err.json);
     }
     res.contentType(type);
     data.pipe(res);
   };
 };
+
+module.exports.noContent = function(res) {
+  return function (err, data) {
+    if (err) {
+      return res.status(err.statusCode).send(err.json);
+    }
+    res.status(204).send();
+  };
+}
