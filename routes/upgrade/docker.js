bebdc57efac76a65054e5aae4b0550ba099639ce
Adrien Folie <folie.adrien@gmail.com>
2015-04-27 17:44:08 +0200

    reindent cascading

--- routes/upgrade/docker.js
+++ routes/upgrade/docker.js
@@ -5,15 +5,15 @@ var express = require('express'),
 let router = express.Router();
 
 router
-  .post('/containers/:id/attach', (req, socket) => {
-    docker
-      .getContainer(req.params.id)
-      .attach(req.query, handler.hijack(socket));
-  })
-  .post('/exec/:id/start', (req, socket) => {
-    docker
-      .getExec(req.params.id)
-      .start(req.query, handler.hijack(socket));
-  });
+.post('/containers/:id/attach', (req, socket) => {
+  docker
+    .getContainer(req.params.id)
+    .attach(req.query, handler.hijack(socket));
+})
+.post('/exec/:id/start', (req, socket) => {
+  docker
+    .getExec(req.params.id)
+    .start(req.query, handler.hijack(socket));
+});
 
 module.exports = router;
