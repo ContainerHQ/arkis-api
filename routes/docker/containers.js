21893d4a68373a53e6123548f5707cd7e072ccc1
Adrien Folie <folie.adrien@gmail.com>
2015-05-04 03:54:39 +0200

    cleanup

--- routes/docker/containers.js
+++ routes/docker/containers.js
@@ -6,13 +6,12 @@ let router = express.Router()
 
 router
 .get('/json', (req, res) => {
-  req.docker.listContainers(req.query, handler.sendTo(res));
+  req.docker.listContainers(req.query, handler.docker(res));
 })
 .post('/create', (req, res) => {
   let opts = _.merge(req.query, req.body);
 
-  req.docker.createContainer(opts, handler.sendTo(res, container => {
-    res.status(201);
+  req.docker.createContainer(opts, handler.docker(res, {status: 201}, container => {
     // TODO: add warnings
     return { Id: container.id };
   }));
@@ -23,66 +22,67 @@ router
   next();
 })
 .get('/:id/export', (req, res) => {
-  req.container.export(handler.streamTo(res, 'application/octed-stream'));
+  req.container.export(handler.docker(res,
+    {stream: true, type: 'application/octed-stream'}
+  ));
 })
 .get('/:id/changes', (req, res) => {
-  req.container.changes(handler.sendTo(res));
+  req.container.changes(handler.docker(res));
 })
 .get('/:id/json', (req, res) => {
-  req.container.inspect(handler.sendTo(res));
+  req.container.inspect(handler.docker(res));
 })
 .get('/:id/top', (req, res) => {
-  req.container.top(req.query, handler.sendTo(res));
+  req.container.top(req.query, handler.docker(res));
 })
 .get('/:id/logs', (req, res) => {
-  req.container.logs(req.query, handler.streamTo(res,
-    'application/vnd.docker.raw-stream'
+  req.container.logs(req.query, handler.docker(res,
+    {stream: true, type: 'application/vnd.docker.raw-stream'}
   ));
 })
 .get('/:id/stats', (req, res) => {
-  req.container.stats(handler.streamTo(res));
+  req.container.stats(handler.docker(res, {stream: true}));
 })
 .post('/:id/attach', (req, res) => {
-  req.container.attach(req.query, handler.hijack(req.socket));
+  req.container.attach(req.query, handler.hijack(res));
 })
 .post('/:id/start', (req, res) => {
-  req.container.start(req.query, handler.noContent(res));
+  req.container.start(req.query, handler.docker(res, {status: 204}));
 })
 .post('/:id/stop', (req, res) => {
-  req.container.stop(req.query, handler.noContent(res));
+  req.container.stop(req.query, handler.docker(res, {status: 204}));
 })
 .post('/:id/kill', (req, res) => {
-  req.container.kill(req.query, handler.noContent(res));
+  req.container.kill(req.query, handler.docker(res, {status: 204}));
 })
 .post('/:id/restart', (req, res) => {
-  req.container.restart(req.query, handler.noContent(res));
+  req.container.restart(req.query,handler.docker(res, {status: 204}));
 })
 .post('/:id/pause', (req, res) => {
-  req.container.pause(req.query, handler.noContent(res));
+  req.container.pause(req.query, handler.docker(res, {status: 204}));
 })
 .post('/:id/unpause', (req, res) => {
-  req.container.unpause(req.query, handler.noContent(res));
+  req.container.unpause(req.query, handler.docker(res, {status: 204}));
 })
 .post('/:id/rename', (req, res) => {
-  req.container.rename(req.query, handler.sendTo(res));
+  req.container.rename(req.query, handler.docker(res));
 })
 .post('/:id/resize', (req, res) => {
-  req.container.resize(req.query, handler.sendTo(res));
+  req.container.resize(req.query, handler.docker(res));
 })
 .post('/:id/wait', (req, res) => {
-  req.container.wait(handler.sendTo(res));
+  req.container.wait(handler.docker(res));
 })
 .post('/:id/copy', (req, res) => {
-  req.container.copy(req.body, handler.streamTo(res));
+  req.container.copy(req.body, handler.docker(res));
 })
 .post('/:id/exec', (req, res) => {
-  req.container.exec(req.body, handler.sendTo(res, exec => {
-    res.status(201);
+  req.container.exec(req.body, handler.docker(res, {status: 201}, exec => {
     return { Id: exec.id };
   }));
 })
 .delete('/:id', (req, res) => {
-  req.container.remove(req.query, handler.noContent(res));
+  req.container.remove(req.query, handler.docker(res, {status: 204}));
 });
 
 module.exports = router;
