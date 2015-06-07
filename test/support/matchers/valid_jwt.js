213907c868609081a03d62df60826154aadb24e1
Adrien Folie <folie.adrien@gmail.com>
2015-06-07 05:29:14 +0200

    add more accurate tests for user

--- test/support/matchers/valid_jwt.js
+++ test/support/matchers/valid_jwt.js
@@ -4,8 +4,8 @@ var jwt = require('jsonwebtoken'),
   secrets = require('../../../config/secrets');
 /*
  * Verify that the decoded json web token includes the
- * user email and that the token is verified with the
- * secret key.
+ * user email address and that the token is verified with
+ * the secret key.
  *
  */
 module.exports = function(model) {
