1a18ca4f27230b5982d7529bfb1f2916ccb65162
Adrien Folie <folie.adrien@gmail.com>
2015-06-11 17:03:18 +0200

    add test for update profile

--- test/integration/v1/update_profile_test.js
+++ test/integration/v1/update_profile_test.js
@@ -23,13 +23,31 @@ describe('PATCH /account/profile', () => {
 
       expect(user.getProfile())
         .to.eventually.have.property('dataValues')
-        .that.deep.equals(profile).and
-        .that.have.property('fullname', fullname)
+          .that.deep.equals(profile).and
+          .that.have.property('fullname', fullname)
         .notify(done);
     });
   });
 
   context('with invalid attributes', done => {
+    it('responds with a bad request status and errors', done => {
+      let fullname = _.repeat('*', 65);
 
+      api.account(user).updateProfile()
+      .field('fullname', fullname)
+      .expect(400)
+      .end((err, res) => {
+        if (err) { return done(err); }
+
+        expect(
+          user.getProfile()
+          .then(profile => {
+            profile.fullname = fullname;
+            return profile.save();
+        }))
+        .to.be.rejectedWith(res.body.errors)
+        .notify(done);
+      });
+    });
   });
 });
