e612378e257d491e4d3704c8b0a2ffc309e4793d
Adrien Folie <folie.adrien@gmail.com>
2015-06-30 19:34:57 +0200

    add istanbul and codeclimate reports

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,7 +9,13 @@ if [ "$GITHUB_CLIENT_ID" = "" ] || [ "$GITHUB_SECRET_KEY" = "" ]; then
     exclude="--grep github --invert"
 fi
 
-# Detect errors and potential problems
+# Detect errors and potential problems.
 jshint app
 
-NODE_ENV=test mocha $exclude $@
+# Launch mocha with istanbul coverage reports.
+NODE_ENV=test istanbul cover _mocha -- $exclude $@
+
+# Upload coverage report to codeclimate.
+if [ $CODECLIMATE_REPO_TOKEN ]; then
+    codeclimate < coverage/lcov.info
+fi
