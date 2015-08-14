cdfa4140dfc27d9ba32b1b978a426c566819d468
Adrien Folie <folie.adrien@gmail.com>
2015-08-14 02:17:42 +0200

    add token decoder unit tests

---
+++ test/middlewares/token_decoder_test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let services = require('../../app/services'),
+  tokenDecoder = require('../../app/middlewares').tokenDecoder;
+
+describe('Token Decoder Middleware', () => {
+  let req;
+
+  beforeEach(() => {
+    req = {};
+  });
+
+  context('when token is a valid jwt', () => {
+    const JIT = random.string();
+
+    let token;
+
+    beforeEach(() => {
+      token = services.token.generate(JIT);
+    });
+
+    it('decodes the given jwt token', done => {
+      tokenDecoder(req, {}, () => {
+        expect(req.token.jit).to.equal(JIT);
+        done();
+      }, token);
+    });
+  });
+
+  context('when token is invalid', () => {
+    it('sends an authorized response', done => {
+      tokenDecoder(req, { unauthorized: done }, null, '');
+    });
+  });
+});
