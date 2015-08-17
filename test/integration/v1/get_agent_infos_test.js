1bd2343f8a1891ef8fe869ffbd5923eca484eb27
Adrien Folie <folie.adrien@gmail.com>
2015-08-17 13:08:24 +0200

    move some agent tests from one scope

--- test/integration/v1/get_agent_infos_test.js
+++ test/integration/v1/get_agent_infos_test.js
@@ -5,38 +5,36 @@ let AgentManager = require('../../../app/services').AgentManager;
 describe('GET /agent/:token/infos', () => {
   db.sync();
 
-  context('when the node exists', () => {
-    let manager, node;
+  let manager, node;
 
+  beforeEach(() => {
+    return factory.buildSync('cluster').save().then(cluster => {
+      node = factory.buildSync('node', { cluster_id: cluster.id });
+      return node.save();
+    }).then(() => {
+      manager = new AgentManager(node);
+    });
+  });
+
+  it('returns the infos required by the node agent', done => {
+    api.agent(node).infos()
+    .expect(200, (err, res) => {
+      if (err) { return done(err); }
+
+      manager.infos().then(infos => {
+        expect(res.body).to.deep.equal(infos);
+        done();
+      }).catch(done);
+    });
+  });
+
+  context('when node no longer exists', () => {
     beforeEach(() => {
-      return factory.buildSync('cluster').save().then(cluster => {
-        node = factory.buildSync('node', { cluster_id: cluster.id });
-        return node.save();
-      }).then(() => {
-        manager = new AgentManager(node);
-      });
+      return node.destroy();
     });
 
-    it('returns the infos required by the node agent', done => {
-      api.agent(node).infos()
-      .expect(200, (err, res) => {
-        if (err) { return done(err); }
-
-        manager.infos().then(infos => {
-          expect(res.body).to.deep.equal(infos);
-          done();
-        }).catch(done);
-      });
-    });
-
-    context('when the node no longer exists', () => {
-      beforeEach(() => {
-        return node.destroy();
-      });
-
-      it('returns a 404 not found', done => {
-        api.agent(node).infos().expect(404, done);
-      });
+    it('returns a 404 not found', done => {
+      api.agent(node).infos().expect(404, done);
     });
   });
 
