36bb8fca74c892ef8a30eee0c242b0a5666da995
Adrien Folie <folie.adrien@gmail.com>
2015-09-19 03:03:36 +0200

    move error middleware in app.js

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-let express = require('express'),
-  errorHandler = require('../../middlewares').errorHandler;
+let express = require('express');
 
 let router = express.Router();
 
 router
-.use('/v1', require('./v1'))
-.use(errorHandler);
+.use('/v1', require('./v1'));
 
 module.exports = router;
